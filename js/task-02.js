8140642fc0093fec74a5c89c11e75ee5348789fa
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-03-12 12:30:41 +0100

    task 2 update

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+for (const ingredient of ingredients) {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.classList.add("ingredientItem");
+  ingredientItem.textContent = ingredient;
+  list.append(ingredientItem);
+
+  console.log(ingredientItem);
+}
