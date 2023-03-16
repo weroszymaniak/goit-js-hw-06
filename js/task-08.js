bb99e1f3262186db55cee6dc679eab1856e300e8
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-03-16 20:40:50 +0100

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Proszę wypełnić wszystkie pola!");
+  }
+
+  const elementObj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  return console.log(elementObj);
+  event.currentTarget.reset();
+}
+form.addEventListener("submit", handleSubmit);
