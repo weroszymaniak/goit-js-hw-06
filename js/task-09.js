368294ccb0e7d329492890790aed9aefebd0c0e8
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-03-17 18:37:22 +0100

    task 9 update

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const buttonChange = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+const changeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+};
+
+buttonChange.addEventListener("click", changeColor);
