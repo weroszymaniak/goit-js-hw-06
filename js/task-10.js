aae15da1a55bf60a6771ee39afe63507dbbe20e2
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-03-18 16:32:14 +0100

    task 10 update

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector("#boxes");
+const createBtn = document.querySelector("#controls button[data-create]");
+const destroyBtn = document.querySelector("#controls button[data-destroy]");
+
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i++) {
+    boxes.insertAdjacentHTML(
+      "beforeend",
+      `<div style="width:${Math.floor(i * 10 + 20)}px; height: ${Math.floor(
+        i * 10 + 20
+      )}px; background-color: ${getRandomHexColor()};"></div`
+    );
+  }
+}
+const handler = () => {
+  const amount = document.querySelector("#controls input[type=number]").value;
+  boxes.innerHTML = null;
+  createBoxes(amount);
+};
+
+const remover = () => {
+  boxes.innerHTML = null;
+};
+
+createBtn.addEventListener("click", handler);
+destroyBtn.addEventListener("click", remover);
